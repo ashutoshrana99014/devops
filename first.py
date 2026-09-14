1dd9684b9c979845b711605eb326cad965288136
ashutoshrana99014 <ashutosh_pa2507mte14@iitp.ac.in>
2026-09-14 14:25:37 +0530

    subtraction program is adds

--- first.py
+++ first.py
@@ -7,4 +7,12 @@ this is a function to calculate the sum of first n numbers
 '''
 print("Sum of first 5 numbers is:",firstcode(0,5))
     
-        
+
+def secondcode(sub,n):
+    for i in range(n):
+        sub -= i
+    return sub
+'''
+this is a function to calculate the subtraction of first n numbers
+'''
+print("Subtraction of first 5 numbers is:",secondcode(0,5)) 
