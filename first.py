9333c419fdc8fe928f2252ca42eebd61f16ebe70
ashutoshrana99014 <ashutosh_pa2507mte14@iitp.ac.in>
2026-09-14 14:11:44 +0530

    add docstrings

--- first.py
+++ first.py
@@ -2,7 +2,9 @@ def firstcode(sum,n):
     for i in range(n):
         sum += i
     return sum
-
+'''
+this is a function to calculate the sum of first n numbers
+'''
 print("Sum of first 5 numbers is:",firstcode(0,5))
     
         
